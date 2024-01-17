9a5e3c7440cd5502df741e7bae29041bb3404a14
Oliver <yplpf96@gmail.com>
2024-01-17 21:47:45 +0800

    发布版本

--- src/Req.php
+++ src/Req.php
@@ -1,43 +1,42 @@
 <?php
 
-/**获取get请求中的参数*/
-function get(string $key, $default = ''): string
+/**获取请求的数据*/
+class Req
 {
-    return $_GET[$key] ?? $default;
+    /**获取get请求中的参数*/
+    public static function get(string $key, $default = ''): string
+    {
+        return $_GET[$key] ?? $default;
+    }
+
+    /**获取post请求中的参数*/
+    public static function post(string $key, $default = ''): string
+    {
+        return $_POST[$key] ?? $default;
+    }
+
+    /**获取session中的参数*/
+    public static function session(string $key, $default = ''): string
+    {
+        return $_SESSION[$key] ?? $default;
+    }
+
+    /**获取cookie中的参数*/
+    public static function cookie(string $key, $default = ''): string
+    {
+        return $_COOKIE[$key] ?? $default;
+    }
+
+    /**获取request中的参数*/
+    public static function request(string $key, $default = ''): string
+    {
+        return $_REQUEST[$key] ?? $default;
+    }
+
+    /**获取本次请求流*/
+    public static function input()
+    {
+        return file_get_contents('php://input');
+    }
+
 }
-
-/**获取post请求中的参数*/
-
-function post(string $key, $default = ''): string
-{
-    return $_POST[$key] ?? $default;
-}
-
-/**获取session中的参数*/
-
-function session(string $key, $default = ''): string
-{
-    return $_SESSION[$key] ?? $default;
-}
-
-/**获取cookie中的参数*/
-
-function cookie(string $key, $default = ''): string
-{
-    return $_COOKIE[$key] ?? $default;
-}
-
-/**获取request中的参数*/
-
-function request(string $key, $default = ''): string
-{
-    return $_REQUEST[$key] ?? $default;
-}
-
-/**获取本次请求流*/
-
-function input()
-{
-    return file_get_contents('php://input');
-}
-$b = "ccc";
