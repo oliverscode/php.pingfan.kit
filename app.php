a93807db4f239fd29bc6a5e62fd6905f7e03a9ad
Oliver <yplpf96@gmail.com>
2024-01-18 02:41:40 +0800

    发布版本

---
+++ app.php
@@ -0,0 +1,28 @@
+<?php
+// windows 删除所有远程tag
+// git tag -l | ForEach-Object { git push origin --delete $_ }
+// 删除所有本地的tag
+// git tag -l | ForEach-Object { git tag -d $_ }
+
+require_once 'vendor/autoload.php';
+$options = [
+    'static' => ['/'],
+    'index'=>'index.php',
+    'debug' => false,
+];
+App::Run($options);
+
+/*
+nginx配置
+
+location ~ ^/(\.user.ini|\.htaccess|\.git|\.svn|\.project|LICENSE|README.md)
+{
+    return 404;
+}
+
+location /  {
+    fastcgi_pass 127.0.0.1:9000;
+    include fastcgi_params;
+    fastcgi_param SCRIPT_FILENAME $document_root/app.php;
+}
+*/
--- src/App.php
+++ src/App.php
@@ -1,96 +1,81 @@
 <?php
-
 class App
 {
+    private static $option = [
+        'debug' => false,
+        'errLevel' => E_ERROR,
+        'index' => '/index.php',
+        'static' => [],
+        'self' => 'app.php',
+    ];
+
     public static function Run($opt = [])
     {
-        $option = [
-            'defaultController' => 'home',
-            'defaultAction' => 'index',
-            'debug' => false,
-            'errLevel' => E_ERROR,
-        ];
         if (is_array($opt)) {
-            $option = array_merge($option, $opt);
+            self::$option = array_merge(self::$option, $opt);
         }
+        error_reporting(self::$option['errLevel']); // 设置错误级别
 
-        error_reporting($option['errLevel']); // 设置错误级别
-
-        // 提取路径中的控制器和方法
-        $url = new Str($_SERVER['REQUEST_URI']);
-        $self = new Str($_SERVER['PHP_SELF']);
-        if ($_SERVER['REQUEST_URI'] . '.php' == $self) {
-//            echo "url:$url->Value.php\n";
-//            echo "self:$self\n";
+        // 默认首页
+        $url = $_SERVER['SCRIPT_NAME'];
+        if ($url == '/') {
+            $url = self::$option['index'];
+        }
+        // 取扩展名
+        $ext = pathinfo($url, PATHINFO_EXTENSION);
+        if ($ext == '') {
+            $ext = 'php';
+            $url .= '.php';
+        } elseif ($ext == 'php' && !self::$option['debug']) {
+            Uri::notfound();
             die;
         }
 
-        $path = $url->match('/^\/[\w\/]+(?=\?|$)/');
-
-
-        if ($url == '/' && strlen($path) == 0) {
-            $path = $option['defaultController'] . '/' . $option['defaultAction'];
-            $path = new Str($path);
-        }
-
-        // 分割控制器和方法
-        $arr = $path->split('/');
-        if (count($arr) == 2) {
-            $controller = $arr[0];
-            $action = $arr[1];
-        }
-
-        $root = dirname($_SERVER['SCRIPT_FILENAME']);
-
-//        echo "root:$root\n";
-
-
-        // 当前脚本的物理目录
-        $fileHide = Uri::combine($root, $path . '.php');
-        $fileApi = Uri::combine($root, $controller . '.php');
-        $file = Uri::combine($root, $url);
-
-//        echo "fileHide:$fileHide\n";
-//        echo "fileApi:$fileApi\n";
-//        echo "file:$file\n";
-//        echo "self:$self\n";
-//        die;
-
-        try {
-
-            if ($option['debug'] && file_exists($file) && strpos($file, '.php') !== false) {
+        // 如果是php文件
+        if ($ext == 'php') {
+            $file = Uri::combineFromServerRoot($url);
+            if (file_exists($file)) {
                 self::runFile($file);
-            } elseif (file_exists($fileHide)) {
-                self::runFile($fileHide);
-            } elseif ($controller && $action && file_exists($fileApi)) {
-                self::runFile($fileApi);
-                // $controller类
-                if (class_exists($controller)) {
-                    self::runApi($controller, $action);
-                }
-
+                die;
             } else {
                 Uri::notfound();
             }
-
-        } catch (Exception $exception) {
-
-            if ($option['debug']) {
-                throw $exception;
-            } else {
-                $log = new Log("error");
-                $log->error($exception->getMessage());
+        } else {
+            // 静态文件
+            foreach (self::$option['static'] as $staticDir) {
+                $file = Uri::combineFromServerRoot($staticDir, $url);
+                if (file_exists($file)) {
+                    $mime = Mime::get($ext);
+                    header("Content-type: $mime");
+                    readfile($file);
+                    die;
+                }
             }
-            Uri::error();
         }
-
-
+        Uri::notfound();
     }
 
     private static function runFile($file)
     {
+        // 获取$file文件名
+        $name = basename($file);
+        if (strcasecmp($name, self::$option['self']) == 0) {
+            Uri::notfound();
+            return;
+        }
         header('Content-Type: text/html; charset=utf-8'); // 设置编码
-        include $file;
+        try {
+            include $file;
+        } catch (Exception $exception) {
+            if (self::$option['debug'] === true) {
+                throw $exception;
+            } else {
+                $log = new Log("error");
+                $logMessage = '异常消息: ' . $exception->getMessage() . ' 在文件 ' . $exception->getFile() . ' 第 ' . $exception->getLine() . ' 行';
+                $log->error($logMessage);
+            }
+            Uri::error();
+        }
     }
 
     private static function runApi($controller, $action)
@@ -110,28 +95,6 @@ class App
         // 调用方法
         $result = $reflectionMethod->invoke($class, ...$params);
         echo $result;
-
     }
 }
 
-/*
-nginx配置
-location ~ \.php$ {
-    if ($request_uri !~* ^/route7624d943f713513c.php) {
-        rewrite ^ /route7624d943f713513c.php last;
-    }
-}
-location ~ ^/(\.user.ini|\.htaccess|\.git|\.svn|\.project|LICENSE|README.md)
-{
-    return 404;
-}
-
-# 处理php文件
-location = /route7624d943f713513c.php {
-    include fastcgi_params;
-    fastcgi_pass   127.0.0.1:9000;
-    fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
-    fastcgi_param  PATH_INFO  $fastcgi_path_info;
-    fastcgi_param  PATH_TRANSLATED  $document_root$fastcgi_path_info;
-}
-*/
